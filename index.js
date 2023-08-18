ac2889969e2f6cc7fb70b1347468574950a555f0
ZtRXR <Zengtudor@outlook.com>
2023-08-18 12:24:20 +0800

    参数调用热搜显示

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const GetHot_1 = require("./GetHot");
-(0, GetHot_1.BiliHot)(50).then(e => {
+let getLimits = 1;
+getLimits = Number.parseInt(process.argv[2]);
+if (Number.isNaN(getLimits)) {
+    throw Error("在运行参数中输入获取大小");
+}
+(0, GetHot_1.BiliHot)(getLimits).then(e => {
     e.forEach((each, index) => {
         console.log(index + 1, each.keyword);
     });
--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
+import { get } from "http";
 import { BiliHot } from "./GetHot";
 
+let getLimits = 1;
 
+getLimits = Number.parseInt(process.argv[2])
 
-BiliHot(50).then(e=>{
+if(Number.isNaN(getLimits)){
+    throw Error("在运行参数中输入获取大小")
+}
+
+BiliHot(getLimits).then(e=>{
     e.forEach((each,index)=>{
         console.log(index+1,each.keyword)
     })
